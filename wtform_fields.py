54eead53110bfbcf2f2a511e404ccdf609610ef9
pauliswalrus <austinbpaul@gmail.com>
2019-09-25 12:49:52 -0400

    form placeholders for student/tutor registration - austin

--- wtform_fields.py
+++ wtform_fields.py
@@ -37,6 +37,41 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("Username already exists. Select a new username")
 
 
+class TutorRegistrationForm(FlaskForm):
+    """ TutorRegistration form """
+
+    username = StringField('username_label', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    firstname = StringField('firstname_label', validators=[InputRequired(message="First name required"), Length(min=4, max=25, message="First name must be between 4 and 25 characters")])
+    lastname = StringField('lastname_label', validators=[InputRequired(message="Last name required"), Length(min=4, max=25, message="Last name must be between 4 and 25 characters")])
+    email = StringField("email_label", validators=[InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    workplace = StringField('workplace_label', validators=[InputRequired("Please enter your workplace."), Length(min=4, max=25, message="Workplace must be between 4 and 25 characters")])
+    occupation = StringField('occupation_label', validators=[InputRequired("Please enter your occupation."), Length(min=4, max=25, message="Occupation must be between 4 and 25 characters")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+    confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
+    submit_button = SubmitField('Create Account')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a new username")
+
+class StudentRegistrationForm(FlaskForm):
+    """ StudentRegistration form """
+
+    username = StringField('username_label', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    firstname = StringField('firstname_label', validators=[InputRequired(message="First name required"), Length(min=4, max=25, message="First name must be between 4 and 25 characters")])
+    lastname = StringField('lastname_label', validators=[InputRequired(message="Last name required"), Length(min=4, max=25, message="Last name must be between 4 and 25 characters")])
+    email = StringField("email_label", validators=[InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    introduction = StringField('intro_label', validators=[InputRequired("Please enter your introduction."), Length(min=4, max=25, message="Intro must be between 4 and 25 characters")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+    confirm_pswd = PasswordField('confirm_pswd_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
+    submit_button = SubmitField('Create Account')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a new username")
+
 
 class LoginForm(FlaskForm):
     """ Login form """
