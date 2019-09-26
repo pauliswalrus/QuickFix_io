1f0bf714a2fa277cad2ec01e531f5f21185998c1
pauliswalrus <austinbpaul@gmail.com>
2019-09-26 15:42:06 -0400

    Delete blogpost from chat window working, moved to app directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
+#secret key - required for socketio - will be changed at deployment
+app.config['SECRET_KEY'] = 'Replace later'
+
+#db connect to postgress - changes at deployment
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eeuyotcvqfkbua:361187423674645ecc0637ec587f8b9d11b12767c50aecf338a298e7dde8e64d@ec2-174-129-18-42.compute-1.amazonaws.com:5432/d4daah96ejr9e0'
--- app/app.py
+++ app/app.py
@@ -7,11 +7,11 @@ from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from app.wtform_fields import *
 from app.dataModel import *
 
-#secret key - required for socketio - will be changed at deployment
-app.config['SECRET_KEY'] = 'Replace later'
-
-#db connect to postgress - changes at deployment
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eeuyotcvqfkbua:361187423674645ecc0637ec587f8b9d11b12767c50aecf338a298e7dde8e64d@ec2-174-129-18-42.compute-1.amazonaws.com:5432/d4daah96ejr9e0'
+# #secret key - required for socketio - will be changed at deployment
+# app.config['SECRET_KEY'] = 'Replace later'
+#
+# #db connect to postgress - changes at deployment
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eeuyotcvqfkbua:361187423674645ecc0637ec587f8b9d11b12767c50aecf338a298e7dde8e64d@ec2-174-129-18-42.compute-1.amazonaws.com:5432/d4daah96ejr9e0'
 
 #db init
 
