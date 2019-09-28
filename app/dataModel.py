09274d66075f9ffe7e354913d6054ccdb380de8d
pauliswalrus <austinbpaul@gmail.com>
2019-09-27 21:44:29 -0400

    post-sprint 3 build - new directory. see structure

--- app/dataModel.py
+++ app/dataModel.py
@@ -14,7 +14,6 @@ db = SQLAlchemy(app)
 class User(UserMixin, db.Model):
     """ User Model """
 
-
     __tablename__ = "users_new"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
