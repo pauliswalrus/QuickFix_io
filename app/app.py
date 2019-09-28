066db8f83c3e1bd552e314962aa017dc643b4ea8
pauliswalrus <austinbpaul@gmail.com>
2019-09-28 07:15:11 -0400

    file upload and download working :)
    refactoring began

--- app/app.py
+++ app/app.py
@@ -312,10 +312,10 @@ def profile():
     return render_template('profile.html', username=current_user.username, firstname=firstname, lastname=lastname,
                            email=email, status_string=status_string, blog_posts=blog_posts, role_name=role_name, file_form=file_form, user_files=user_files)
 
-@app.route('/download')
-def download():
+@app.route('/download/<string:dl_name>/')
+def download(dl_name):
     #file_data = FileUpload.query.first()
-    file_data = FileUpload.query.filter_by(username=current_user.username).first()
+    file_data = FileUpload.query.filter_by(file_name=dl_name).first()
 
     return send_file(BytesIO(file_data.data), attachment_filename=file_data.file_name, as_attachment=True)
 
