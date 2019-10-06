d2b4b316b7618cde35ea11a9f9de726f79ae97e5
pauliswalrus <austinbpaul@gmail.com>
2019-10-06 12:33:57 -0400

    Oct 5
    Sprint 4 Turn In Build

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Replace later'
 
 #db connect to postgress - changes at deployment
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qotdqbrkicixgz:fbb6c132f1f8841a4f89a33178683b0792c5444cf35e578596a0170f7764ea35@ec2-174-129-220-12.compute-1.amazonaws.com:5432/d158l9bo7mprq0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qotdqbrkicixgz:fbb6c132f1f8841a4f89a33178683b0792c5444cf35e578596a0170f7764ea35@ec2-174-129-220-12.compute-1.amazonaws.com:5432/d158l9bo7mprq0'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
